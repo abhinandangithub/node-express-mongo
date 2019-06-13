b238d41a5aeb2ab74516411f159f03a1f77ad11f
abhinandan <abhinandan@DESKTOP-355VM6L>
2019-06-13 21:43:32 +0530

    first

---
+++ server/api/user/userRoutes.js
@@ -0,0 +1,12 @@
+var router = require('express').Router();
+var logger = require('../../util/logger');
+
+// setup boilerplate route jsut to satisfy a request
+// for building
+router.param('id', function(){});
+
+router.route('/')
+
+router.route('/:id')
+
+module.exports = router;
